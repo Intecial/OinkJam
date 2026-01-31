7e6c722244278492c768ce40f132e19d628ad8bf
Louis Juliano <ljuliano30@gmail.com>
2026-01-31 11:11:50 +0800

    Player Movement Completed

--- Assets/Player/PlayerMovementController.cs
+++ Assets/Player/PlayerMovementController.cs
@@ -6,30 +6,42 @@ public class PlayerMovementController : MonoBehaviour
     
     
     private Vector2 moveInput;
+    [SerializeField]
+    private Transform player;
 
     [SerializeField]
     private float moveSpeed = 5f;
+    public float rotateSpeed = 720f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
-
+        
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (moveInput.sqrMagnitude > 0.001f)
+        {
+            float angle = Mathf.Atan2(moveInput.y, moveInput.x) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(0f, 0f, angle - 90f);
+            player.transform.rotation = Quaternion.RotateTowards(
+                player.transform.rotation,
+                targetRotation,
+                rotateSpeed * Time.deltaTime
+            );
+        }
     }
 
     public void OnMove(InputAction.CallbackContext context){
         
         moveInput = context.ReadValue<Vector2>();
-
+        Debug.Log(moveInput);
     }
 
     void FixedUpdate(){
-        Vector3 movement = new Vector3(moveInput.x, 0, moveInput.y);
-        transform.position += movement * moveSpeed * Time.deltaTime;
+        Vector3 movement = new Vector3(moveInput.x,  moveInput.y, 0);
+        player.transform.position += movement * moveSpeed * Time.deltaTime;
     }
 }
